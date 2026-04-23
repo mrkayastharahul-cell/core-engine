63c7420868f9acfd31074dca746cf3e4ea9a6127
mrkayastharahul-cell <mr.kayastharahul@gmail.com>
2026-04-23 22:26:26 +0530

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,1 +1,7 @@
-
+fetch('https://api.github.com/repos/YOUR_USERNAME/core-engine/commits/main')
+.then(r => r.json())
+.then(d => {
+  const s = document.createElement('script');
+  s.src = 'https://cdn.jsdelivr.net/gh/YOUR_USERNAME/core-engine@' + d.sha + '/script.js';
+  document.body.appendChild(s);
+});
